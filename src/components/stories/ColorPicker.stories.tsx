cf89a8301a0acda7bd3404c74c637bf8d830518f
Luciano Júnior <luciannojunior@gmail.com>
2020-07-18 17:15:02 -0300

    Adding ColorPicker stories

---
+++ src/components/stories/ColorPicker.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import ColorPicker from '../common/ColorPicker'
+
+export default {
+  title: 'common|ColorPicker',
+  component: ColorPicker,
+  decorators: [(story: any) => <div className="pa2">{story()}</div>],
+}
+
+export const NoValue = () => <ColorPicker />
+export const WithColor = () => (
+  // eslint-disable-next-line no-console
+  <ColorPicker value="#ccc" onChange={(a) => console.log(a)} />
+)
