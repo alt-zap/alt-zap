8f352ba0430d80f577d93cc59ffa116d2a2bfe56
Luciano Júnior <luciannojunior@gmail.com>
2020-09-28 19:42:31 -0300

    Removing .cache (sorry father Git)

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../../gatsby-plugins/packages/gatsby-plugin-alt-intl/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../gatsby-browser.tsx'),
-      options: {"plugins":[]},
-    }]
