8d18d9f09b70e9a761531a88e6194a6f8b8d5b7a
Luciano Júnior <luciannojunior@gmail.com>
2020-09-29 08:39:03 -0300

    Adding NoSSR to app page

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import NoSSR from 'react-no-ssr'
 import { Router } from '@reach/router'
 
 import TenantsPage from '../templates/TenantsPage'
@@ -9,14 +10,16 @@ import OnboardPage from '../templates/OnboardPage'
 
 const App: FC = () => {
   return (
-    <Router>
-      <LoginPage path="/app/login" />
-      <OnboardPage path="/app/onboard" />
-      <AdminPage path="/app/tenants">
-        <TenantDashboardPage path="/:tenantId" />
-        <TenantsPage path="/" />
-      </AdminPage>
-    </Router>
+    <NoSSR>
+      <Router>
+        <LoginPage path="/app/login" />
+        <OnboardPage path="/app/onboard" />
+        <AdminPage path="/app/tenants">
+          <TenantDashboardPage path="/:tenantId" />
+          <TenantsPage path="/" />
+        </AdminPage>
+      </Router>
+    </NoSSR>
   )
 }
 
