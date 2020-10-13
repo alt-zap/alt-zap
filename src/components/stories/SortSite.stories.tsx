f4f979b60ad182e63f66ed05741ad9779a56ac7f
Luciano Júnior <luciannojunior@gmail.com>
2020-10-12 21:30:20 -0300

    Renaming stories file

--- src/components/stories/SortSite.stories.tsx
+++ src/components/stories/SortSite.stories.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import SortCategories from '../tenant/sites/cms/SortSite'
+import SortSite from '../tenant/sites/cms/SortSite'
 import WithAltburguer from './withAltburguer'
 
 export default {
-  title: 'tenant/SortCategories',
-  component: SortCategories,
+  title: 'tenant/SortSite',
+  component: SortSite,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   decorators: [(story: any) => <div className="pa2">{story()}</div>],
 }
 
 export const initial = () => (
   <WithAltburguer>
-    <SortCategories />
+    <SortSite />
   </WithAltburguer>
 )
