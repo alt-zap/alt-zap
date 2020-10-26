aa1fcfed15a84ecf51228cb299f7cef9a93a5924
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-10-26 09:23:15 -0300

    Implements switch button in sort categories

--- src/components/tenant/sites/cms/sort/SortCategories.tsx
+++ src/components/tenant/sites/cms/sort/SortCategories.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useState, useMemo } from 'react'
+import { Switch } from 'antd'
 
 import {
   useTenant,
   countProductPerCategory,
 } from '../../../../../contexts/TenantContext'
 import { useAltIntl } from '../../../../../intlConfig'
+import { Section } from '../../../../../typings'
 import SortableList from './SortableList'
 
 type Props = {
@@ -14,10 +16,11 @@ type Props = {
 const SortCategories: FC<Props> = ({ onSortedCategories }) => {
   const [{ tenant, products }] = useTenant()
   const intl = useAltIntl()
+  const [isVisible, setIsVisible] = useState(true)
 
   // Not using the `visible` prop now, as we will implement it later
-  const [categoryIds, setIds] = useState<number[]>(
-    (tenant?.sites?.zap.categoryIds ?? []).map(({ element }) => element)
+  const [categoryIds, setIds] = useState<Array<Section<number>>>(
+    tenant?.sites?.zap.categoryIds ?? [] // session
   )
 
   // Used to get the products' count. It'd be great to have this on the Context, as we already
@@ -35,21 +38,37 @@ const SortCategories: FC<Props> = ({ onSortedCategories }) => {
   return (
     <SortableList
       list={categoryIds}
-      getIdFromItem={(item) => `${item}`}
+      getIdFromItem={(item) => `${item.element}`}
       renderItem={(item) => (
-        <div className="flex flex-column">
-          <span className="fw6 f5">{tenant?.categories?.[item]?.name}</span>
-          <span className="light-silver">
-            {intl.formatMessage(
-              { id: 'tenant.categories.productCount' },
-              { count: `${productsCount[item]}` }
-            )}
-          </span>
+        <div className="flex  items-center justify-center">
+          <div className="flex flex-column items-baseline">
+            <span className="fw6 f5">
+              {tenant?.categories?.[item.element]?.name}
+            </span>
+            <span className="light-silver">
+              {intl.formatMessage(
+                { id: 'tenant.categories.productCount' },
+                { count: `${productsCount[item.element]}` }
+              )}
+            </span>
+          </div>
+          <Switch
+            className="ml4"
+            checkedChildren={() => {
+              setIsVisible(isVisible)
+              item.visible = isVisible
+            }}
+            unCheckedChildren={() => {
+              setIsVisible(!isVisible)
+              item.visible = isVisible
+            }}
+            defaultChecked
+          />
         </div>
       )}
       onSortedList={(ids) => {
         setIds(ids)
-        onSortedCategories(ids)
+        onSortedCategories(ids.map(({ element }) => element))
       }}
     />
   )
