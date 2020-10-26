b8fed42c379811b424f352f29ba5727420557810
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-10-26 09:23:56 -0300

    Implements switch button in sort products

--- src/components/tenant/sites/cms/sort/SortProducts.tsx
+++ src/components/tenant/sites/cms/sort/SortProducts.tsx
@@ -20,6 +20,7 @@ const SortProducts: FC<Props> = ({
 }) => {
   const [{ products, tenant }] = useTenant()
   const intl = useAltIntl()
+  const [isVisible, setIsVisible] = useState(true)
 
   // Not using the `visible` prop now, as we will implement it later
   const [productIds, setIds] = useState<Array<Section<string>>>(
