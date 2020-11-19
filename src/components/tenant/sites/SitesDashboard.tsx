5adb111c6615b334647cbcaec756dc9d96b400c6
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2020-11-19 10:22:21 -0300

    Adds new adjustments for QRCode

--- src/components/tenant/sites/SitesDashboard.tsx
+++ src/components/tenant/sites/SitesDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react'
 import { Divider, Table, Tag, Skeleton, Modal, Button } from 'antd'
-import { EditOutlined, EyeOutlined } from '@ant-design/icons'
+import { EditOutlined, QrcodeOutlined, LinkOutlined } from '@ant-design/icons'
 import QRCode from 'qrcode.react'
 
 import { Message, useAltIntl, AltMessage } from '../../../intlConfig'
@@ -26,18 +26,20 @@ const SitesDashboard: FC = () => {
             pagination={false}
             columns={[
               {
-                title: intl.formatMessage({ id: 'tenant.sites.address' }),
+                title: intl.formatMessage({ id: 'tenant.sites' }),
                 dataIndex: 'address',
                 key: 'address',
                 // eslint-disable-next-line react/display-name
                 render: (address) => (
-                  <a
-                    href={`https://${address}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {address}
-                  </a>
+                  <>
+                    <a
+                      href={`https://${address.url}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {address.menu}
+                    </a>
+                  </>
                 ),
               },
               {
@@ -67,10 +69,34 @@ const SitesDashboard: FC = () => {
                   />
                 ),
               },
+              {
+                title: intl.formatMessage({ id: 'qrCode' }),
+                key: 'qrcode',
+                // eslint-disable-next-line react/display-name
+                render: () => (
+                  <Button
+                    onClick={() => {
+                      setShowQR(true)
+                    }}
+                    icon={<QrcodeOutlined />}
+                  />
+                ),
+              },
             ]}
             dataSource={[
               {
-                address: `${tenant?.slug}.alt.app.br`,
+                address: {
+                  url: `${tenant?.slug}.alt.app.br`,
+                  menu: 'Menu Alt',
+                },
+                status: 'active',
+                key: 'alt-zap',
+              },
+              {
+                address: {
+                  url: `${tenant?.slug}.alt.app.br/loja-fisica/${tenantId}`,
+                  menu: 'Menu Loja Física',
+                },
                 status: 'active',
                 key: 'alt-zap',
               },
@@ -87,68 +113,6 @@ const SitesDashboard: FC = () => {
       >
         <SortSite />
       </Modal>
-      <div className="w-90 w-50-l bg-white mv2 ml4-l ml-0 pb3 ph3 br1">
-        <Divider>
-          <Message id="physicStore" />
-        </Divider>
-        {loading && <Skeleton active />}
-        {!loading && (
-          <Table
-            pagination={false}
-            columns={[
-              {
-                title: intl.formatMessage({ id: 'tenant.sites.address' }),
-                dataIndex: 'address',
-                key: 'address',
-                // eslint-disable-next-line react/display-name
-                render: (address) => (
-                  <a
-                    href={`https://${address}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {address}
-                  </a>
-                ),
-              },
-              {
-                title: intl.formatMessage({ id: 'tenant.sites.status' }),
-                dataIndex: 'status',
-                key: 'status',
-                // eslint-disable-next-line react/display-name
-                render: (status) => (
-                  <Tag
-                    color={status === 'inactive' ? 'volcano' : 'green'}
-                    key={status}
-                  >
-                    <Message id={`tenant.sites.${status}` as AltMessage} />
-                  </Tag>
-                ),
-              },
-              {
-                title: intl.formatMessage({ id: 'qrCode' }),
-                key: 'qrcode',
-                // eslint-disable-next-line react/display-name
-                render: () => (
-                  <Button
-                    onClick={() => {
-                      setShowQR(true)
-                    }}
-                    icon={<EyeOutlined />}
-                  />
-                ),
-              },
-            ]}
-            dataSource={[
-              {
-                address: `portal.alt.app.br/loja-fisica/${tenantId}`,
-                status: 'active',
-                key: 'alt-zap',
-              },
-            ]}
-          />
-        )}
-      </div>
       <Modal
         destroyOnClose
         title={<Message id="qrCode" />}
@@ -157,7 +121,7 @@ const SitesDashboard: FC = () => {
         footer={null}
       >
         <QRCode
-          value={`portal.alt.app.br/loja-fisica/${tenantId}`}
+          value={`${tenant?.slug}.alt.app.br/loja-fisica`}
           renderAs="svg"
           size={240}
           includeMargin
